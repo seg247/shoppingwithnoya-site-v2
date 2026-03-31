1d7a506c211caf2269c2105bb2b409c4ffa796f5
seg247 <seg247@users.noreply.github.com>
2026-03-31 15:06:57 -0400

    Add custom domain: deals.shoppingwithnoya.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://seg247.github.io',
-  base: '/shoppingwithnoya-site-v2',
+  site: 'https://deals.shoppingwithnoya.com',
+  base: '/',
   integrations: [sitemap()],
   output: 'static',
 });
